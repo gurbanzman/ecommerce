56e10c004359bdc7316cf0e3363c1cab1c8b4619
Murad Gurbanzada <gurbanzadamurad@gmail.com>
2025-05-01 23:42:17 +0400

    07: trpc integration fix

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -1,12 +1,13 @@
 'use client';
 // ^-- to make sure we can mount the Provider from a server component
+import { useState } from 'react';
 import type { QueryClient } from '@tanstack/react-query';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { createTRPCClient, httpBatchLink } from '@trpc/client';
 import { createTRPCContext } from '@trpc/tanstack-react-query';
-import { useState } from 'react';
 import { makeQueryClient } from './query-client';
 import type { AppRouter } from './routers/_app';
+import superjson from 'superjson';
 export const { TRPCProvider, useTRPC } = createTRPCContext<AppRouter>();
 let browserQueryClient: QueryClient;
 function getQueryClient() {
@@ -42,7 +43,7 @@ export function TRPCReactProvider(
     createTRPCClient<AppRouter>({
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: superjson,
           url: getUrl(),
         }),
       ],
