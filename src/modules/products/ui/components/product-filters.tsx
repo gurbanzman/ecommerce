9a0ab256474c24f0e4b74f0dd9ea5be5fd59f249
Murad Gurbanzada <gurbanzadamurad@gmail.com>
2025-05-08 20:44:21 +0400

    API filters sorting

--- src/modules/products/ui/components/product-filters.tsx
+++ src/modules/products/ui/components/product-filters.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { ChevronDownIcon, ChevronRightIcon } from "lucide-react";
+import { ChevronDownIcon, ChevronRightIcon, Tags } from "lucide-react";
 import { useState } from "react";
 import { PriceFilter } from "./price-filter";
 import { useProductFilters } from "../../hooks/use-product-filters";
+import { TagsFilter } from "./tags-filter";
 
 interface ProductFilterProps {
   title: string;
@@ -34,6 +35,30 @@ const ProductFilter = ({ title, className, children }: ProductFilterProps) => {
 export const ProductFilters = () => {
   const [filters, setFilters] = useProductFilters();
 
+  const hasAnyFilters = Object.entries(filters).some(([key, value]) => {
+
+    if(key === "sort"){
+      return false;
+    }
+
+    if(Array.isArray(value)){
+      return value.length > 0;
+    }
+
+    if (typeof value === "string") {
+      return value !== "";
+    }
+    return value !== null;
+  });
+
+  const onClear = () => {
+    setFilters({
+      minPrice: "",
+      maxPrice: "",
+      tags: [],
+    });
+  };
+
   const onChange = (key: keyof typeof filters, value: unknown) => {
     setFilters({ ...filters, [key]: value });
   };
@@ -42,13 +67,15 @@ export const ProductFilters = () => {
     <div className="border rounded-md bg-white">
       <div className="p-4 border-b flex items-center justify-between">
         <p className="font-medium">Filters</p>
-        <button
-          type="button"
-          className="underline cursor-pointer"
-          onClick={() => console.log("Clear filters")}
-        >
-          Clear
-        </button>
+        {hasAnyFilters && (
+          <button
+            type="button"
+            className="underline cursor-pointer"
+            onClick={onClear}
+          >
+            Clear
+          </button>
+        )}
       </div>
       <ProductFilter title="Price" className="border-b-0">
         <PriceFilter
@@ -56,7 +83,14 @@ export const ProductFilters = () => {
           maxPrice={filters.maxPrice}
           onMinPriceChange={(value) => onChange("minPrice", value)}
           onMaxPriceChange={(value) => onChange("maxPrice", value)}
-        />  
+        />
+      </ProductFilter>
+
+      <ProductFilter title="Tags" className="border-b-0">
+        <TagsFilter
+          tags={filters.tags}
+          onChange={(tags) => onChange("tags", tags)}
+        />
       </ProductFilter>
     </div>
   );
